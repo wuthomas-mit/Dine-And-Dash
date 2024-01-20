f58eabd779115c53c506133c4d1a64f868e027d1
puppyswan <92048050+puppyswan@users.noreply.github.com>
2024-01-20 17:25:28 -0500

    basic-home

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -8,20 +8,32 @@ import "../css/Skeleton.css";
 const GOOGLE_CLIENT_ID = "728466374633-tela4eovuk3jdagsce8gbsk20v6m6f1p.apps.googleusercontent.com";
 
 const Home = ({ userId, handleLogin, handleLogout }) => {
+  const handlePlayLocal = () => {
+    console.log("New Button Clicked");
+  };
+  const handlePlayOnline = () => {
+    console.log("New Button Clicked");
+  };
   return (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      {userId ? (
-        <button
-          onClick={() => {
-            googleLogout();
-            handleLogout();
-          }}
-        >
-          Logout
-        </button>
-      ) : (
-        <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
-      )}
+      <div className="flex-container">
+        {userId ? (
+          <button
+            onClick={() => {
+              googleLogout();
+              handleLogout();
+            }}
+          >
+            Logout
+          </button>
+        ) : (
+          <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
+        )}
+        <div className="buttons-container">
+          <button onClick={handlePlayLocal}>Play Local</button>
+          <button onClick={handlePlayOnline}>Play Online</button>
+        </div>
+      </div>
     </GoogleOAuthProvider>
   );
 };
---
+++ client/src/components/pages/Start.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Start = () => {
+  return <div>Start Page Content</div>;
+};
+
+export default Start;
