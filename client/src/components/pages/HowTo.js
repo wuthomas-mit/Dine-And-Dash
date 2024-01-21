7f4b9c36a2fa5dd50e2bc4626532ee7278070cbf
puppyswan <92048050+puppyswan@users.noreply.github.com>
2024-01-20 20:19:31 -0500

    update-.1

---
+++ client/src/components/pages/HowTo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import "../../utilities.css";
+
+const Start = () => {
+  const navigate = useNavigate();
+  const handleHome = () => {
+    navigate("/");
+  };
+  return (
+    <div className="flex-container">
+      {
+        <div className="buttons-container">
+          <button onClick={handleHome}>Home</button>
+        </div>
+      }
+    </div>
+  );
+};
+
+export default Start;
