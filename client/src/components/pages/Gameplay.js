89c3f7a96df3e1f3fd02199c4db64d0aebe8d8a6
Krystal Montgomery <kryst@my-baby.mit.edu>
2024-01-22 03:52:51 -0500

    play/pause and stop work, but timer doesn't start on render

--- client/src/components/pages/Gameplay.js
+++ client/src/components/pages/Gameplay.js
@@ -6,16 +6,36 @@ import "../../utilities.css";
 
 const Gameplay = () => {
   const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  // useEffect for map initialization
   useEffect(() => {
     initializeMap();
-    // Cleanup function for when the component unmounts
-    const timer = setInterval(() => {
-      setTime((prevTime) => prevTime + 1);
-    }, 1000);
-    return () => {
-      clearInterval(timer);
-    };
   }, []);
+
+  // useEffect for timer management
+  useEffect(() => {
+    let timer;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, [isRunning]);
+
+  const handlePlayPause = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
   const formattedTime = `${Math.floor(time / 60)
     .toString()
     .padStart(2, "0")}:${(time % 60).toString().padStart(2, "0")}`;
@@ -25,8 +45,12 @@ const Gameplay = () => {
       <div id="map"></div>
       <div className="timer-container">
         <div className="timer">{formattedTime}</div>
-        <div className="buttons">||</div>
-        <div className="buttons">[]</div>
+        <div className="buttons" onClick={handlePlayPause}>
+          {isRunning ? "||" : ">"}
+        </div>
+        <div className="buttons" onClick={handleStop}>
+          []
+        </div>
       </div>
     </div>
   );
