3bc00ad60bf08f28b1ae007ed13782b49a4da3c0
Krystal Montgomery <kryst@my-baby.mit.edu>
2024-01-22 04:07:11 -0500

    created new Timer.js file to store file logic, to keep Gameplay clear

--- client/src/components/pages/Gameplay.js
+++ client/src/components/pages/Gameplay.js
@@ -1,57 +1,20 @@
 import React, { useEffect, useState } from "react";
 import initializeMap from "./initializeMap";
+import TimerComponent from "./utilities/Timer";
 
 import "../css/Gameplay.css";
 import "../../utilities.css";
 
 const Gameplay = () => {
-  const [time, setTime] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
-
   // useEffect for map initialization
   useEffect(() => {
     initializeMap();
   }, []);
 
-  // useEffect for timer management
-  useEffect(() => {
-    let timer;
-    if (isRunning) {
-      timer = setInterval(() => {
-        setTime((prevTime) => prevTime + 1);
-      }, 1000);
-    }
-
-    return () => {
-      if (timer) clearInterval(timer);
-    };
-  }, [isRunning]);
-
-  const handlePlayPause = () => {
-    setIsRunning(!isRunning);
-  };
-
-  const handleStop = () => {
-    setIsRunning(false);
-    setTime(0);
-  };
-
-  const formattedTime = `${Math.floor(time / 60)
-    .toString()
-    .padStart(2, "0")}:${(time % 60).toString().padStart(2, "0")}`;
-
   return (
     <div className="game-container">
       <div id="map"></div>
-      <div className="timer-container">
-        <div className="timer">{formattedTime}</div>
-        <div className="buttons" onClick={handlePlayPause}>
-          {isRunning ? "||" : ">"}
-        </div>
-        <div className="buttons" onClick={handleStop}>
-          []
-        </div>
-      </div>
+      <TimerComponent/>
     </div>
   );
 };
---
+++ client/src/components/pages/utilities/Timer.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+
+const timerContainerStyle = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  position: "absolute",
+  left: "50%",
+  transform: "translate(-50%)",
+  width: "180px",
+  height: "48px",
+  backgroundColor: "#FCF6E7",
+  border: "4px solid #DD4F3D",
+  borderRadius: "5px",
+  zIndex: 10, 
+}
+
+const timerStyle = {
+  // backgroundColor: 'rgba(255, 255, 255, 0.8)',
+  borderRadius: "5px",
+  zIndex: 10,
+  width: "100px",
+  fontSize: "32px",
+  fontWeight: 700,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  textAlign: "center",
+  overflow: "hidden",
+};
+
+const buttonStyle = {
+  marginLeft: "10px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+function TimerComponent() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  // useEffect for timer management
+  useEffect(() => {
+    let timer;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, [isRunning]);
+
+  const handlePlayPause = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  const formattedTime = `${Math.floor(time / 60)
+    .toString()
+    .padStart(2, "0")}:${(time % 60).toString().padStart(2, "0")}`;
+
+  return (
+    <div style={timerContainerStyle}>
+      <div style={timerStyle}>{formattedTime}</div>
+      <div style={buttonStyle} onClick={handlePlayPause}>
+        {isRunning ? "||" : ">"}
+      </div>
+      <div style={buttonStyle} onClick={handleStop}>[]</div>
+    </div>
+  );
+}
+
+export default TimerComponent;
