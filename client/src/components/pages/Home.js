7a01ab8cbfa1d5a462a3145bf155d5daede7a473
puppyswan <92048050+puppyswan@users.noreply.github.com>
2024-01-30 22:19:53 -0500

    added buttons to howTo

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -81,7 +81,7 @@ const Home = ({ userId, handleLogin, handleLogout }) => {
           }
         });
       }, 100);
-    }, 2000);
+    }, 1000);
   };
 
   // Function to start the second subtitle timer
@@ -144,10 +144,10 @@ const Home = ({ userId, handleLogin, handleLogout }) => {
                 navigate("/howto");
               }}
             >
-              Mission Brief
+              Instructions
             </button>
             <button className="button" onClick={handleGame}>
-              Go Time
+              Begin Heist
             </button>
           </div>
         </div>
